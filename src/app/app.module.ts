eec08b5a4d03971c48631f8d5d39be2c56f65ecf
Tiago Parente <tiago.parente@interativanet.com.br>
2021-12-02 14:17:45 -0300

    Install PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { LoginModule } from './pages/login/login.module';
 import { IndexModule } from './pages/index/index.module';
 import { NgZorroAntdModule } from './ng-zorro-antd/ng-zorro-antd.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 registerLocaleData(pt);
 
@@ -35,7 +37,13 @@ registerLocaleData(pt);
 
     LoginModule,
     IndexModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{ provide: NZ_I18N, useValue: pt_BR }],
   bootstrap: [AppComponent]
